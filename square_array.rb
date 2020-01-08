64b9122e196fe4b87d0c69b45b3cf0b0279dcd18
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-08 01:11:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(numbers = [1,2,3])
 # your code here
+  squared_numbers = []
   numbers.each do |squared|
     squared *= squared
+    squared_numbers << squared
+  end
+  return square_array
 end
